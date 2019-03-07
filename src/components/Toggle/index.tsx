8de53930ccf3412cadfd58f96127f09ff522a98f
John Fazioli <john.fazioli@namecheap.com>
2019-03-07 14:22:44 +0100

    fix background

--- src/components/Toggle/index.tsx
+++ src/components/Toggle/index.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useRef} from "react";
+import React, { FunctionComponent, useRef } from "react";
 import styled from "styled-components";
 import defaultTheme from "../../theme/theme";
 
@@ -58,11 +58,11 @@ interface Props {
    * Height of component
    */
   height?: string;
-  
+
   // ---------------------------------------------------------------
   // Border
   // ---------------------------------------------------------------
-  
+
   /**
    * Border width
    */
@@ -83,7 +83,7 @@ interface Props {
   // ---------------------------------------------------------------
   // Background color
   // ---------------------------------------------------------------
-  
+
   /**
    * Background color for both knob position. This props will override leftBackgroundColor and rightBackgroundColor props
    */
@@ -155,21 +155,21 @@ const ToggleContainer = styled.label``;
 const ToggleBase = styled.span<Props>`
   position: relative;
   box-sizing: border-box;
-  display:inline-grid;
+  display: inline-grid;
   align-items: center;
   width: ${p => p.width || (p.theme && p.theme.width) || defaultTheme.width};
   height: ${p => p.height || (p.theme && p.theme.height) || defaultTheme.height};
   vertical-align: text-top;
   margin: 0 4px;
-  
-  input[type="checkbox"]  {
-      position: absolute;
-      margin-left: -9999px;
-      visibility: hidden;
-      
+
+  input[type="checkbox"] {
+    position: absolute;
+    margin-left: -9999px;
+    visibility: hidden;
+
     // off state
     & + label {
-      display:inline-grid;
+      display: inline-grid;
       box-sizing: border-box;
       align-items: center;
       outline: none;
@@ -177,93 +177,102 @@ const ToggleBase = styled.span<Props>`
       width: ${p => p.width || (p.theme && p.theme.width) || defaultTheme.width};
       height: ${p => p.height || (p.theme && p.theme.height) || defaultTheme.height};
       background-color: ${p =>
-  p.borderColor || p.leftBorderColor || (p.theme && p.theme.leftBorderColor) || defaultTheme.leftBorderColor};
-      border-radius:${p => p.radius || (p.theme && p.theme.radius) || defaultTheme.radius};
-      cursor:pointer;
-      transition : background ease-out 0.3s;
-      
+        p.borderColor || p.leftBorderColor || (p.theme && p.theme.leftBorderColor) || defaultTheme.leftBorderColor};
+      border-radius: ${p => p.radius || (p.theme && p.theme.radius) || defaultTheme.radius};
+      cursor: pointer;
+      transition: background ease-out 0.3s;
+
       &:before {
         content: "";
         display: block;
         position: absolute;
-        border-radius:${p => p.radiusBackground || (p.theme && p.theme.radiusBackground) || defaultTheme.radiusBackground};
-        width: calc(${p => p.width || (p.theme && p.theme.width) || defaultTheme.width} - 2*${p =>
-  p.borderWidth || (p.theme && p.theme.borderWidth) ||
-  defaultTheme.borderWidth});
-        height: calc(${p => p.height || (p.theme && p.theme.height) || defaultTheme.height} - 2*${p =>
-  p.borderWidth || (p.theme && p.theme.borderWidth) ||
-  defaultTheme.borderWidth});
-        background-color: ${p => p.backgroundColor || (p.theme && p.theme.backgroundColor) || defaultTheme.backgroundColor};
-        left:${p => p.borderWidth || (p.theme && p.theme.borderWidth) || defaultTheme.borderWidth};
+        border-radius: ${p =>
+          p.radiusBackground || (p.theme && p.theme.radiusBackground) || defaultTheme.radiusBackground};
+        width: calc(
+          ${p => p.width || (p.theme && p.theme.width) || defaultTheme.width} - 2 *
+            ${p => p.borderWidth || (p.theme && p.theme.borderWidth) || defaultTheme.borderWidth}
+        );
+        height: calc(
+          ${p => p.height || (p.theme && p.theme.height) || defaultTheme.height} - 2 *
+            ${p => p.borderWidth || (p.theme && p.theme.borderWidth) || defaultTheme.borderWidth}
+        );
+        background-color: ${p =>
+          p.backgroundColor ||
+          p.leftBackgroundColor ||
+          (p.theme && p.theme.leftBackgroundColor) ||
+          defaultTheme.leftBackgroundColor};
+        left: ${p => p.borderWidth || (p.theme && p.theme.borderWidth) || defaultTheme.borderWidth};
       }
 
       &:after {
-        display:block;
-        position:absolute;
+        display: block;
+        position: absolute;
         content: "";
         width: ${p => p.knobWidth || (p.theme && p.theme.knobWidth) || defaultTheme.knobWidth};
         height: ${p => p.knobHeight || (p.theme && p.theme.knobHeight) || defaultTheme.knobHeight};
         border-radius: ${p => p.knobRadius || (p.theme && p.theme.knobRadius) || defaultTheme.knobRadius};
         background-color: ${p =>
-  p.knobColor || p.leftKnobColor || (p.theme && p.theme.leftKnobColor) || defaultTheme.leftKnobColor};
-        transition : all ease-out 0.4s;
-        margin-left:${p => p.knobGap || (p.theme && p.theme.knobGap) || defaultTheme.knobGap};
+          p.knobColor || p.leftKnobColor || (p.theme && p.theme.leftKnobColor) || defaultTheme.leftKnobColor};
+        transition: all ease-out 0.4s;
+        margin-left: ${p => p.knobGap || (p.theme && p.theme.knobGap) || defaultTheme.knobGap};
       }
     }
-    
+
     // on state
     &:checked {
       & + label {
         background-color: ${p =>
-  p.borderColor ||
-  p.rightBorderColor ||
-  (p.theme && p.theme.rightBorderColor) ||
-  defaultTheme.rightBorderColor};
-        
+          p.borderColor ||
+          p.rightBorderColor ||
+          (p.theme && p.theme.rightBorderColor) ||
+          defaultTheme.rightBorderColor};
+
         &:before {
           background-color: ${p =>
-  p.backgroundColor ||
-  p.rightBackgroundColor ||
-  (p.theme && p.theme.rightBackgroundColor) ||
-  defaultTheme.rightBackgroundColor};
+            p.backgroundColor ||
+            p.rightBackgroundColor ||
+            (p.theme && p.theme.rightBackgroundColor) ||
+            defaultTheme.rightBackgroundColor};
         }
-        
+
         &:after {
-          margin-left:calc(100% - ${p =>
-  p.knobWidth || (p.theme && p.theme.knobWidth) || defaultTheme.knobWidth} - ${p => p.knobGap || (p.theme && p.theme.knobGap) || defaultTheme.knobGap});
-          transition : all ease-out 0.2s;
+          margin-left: calc(
+            100% - ${p => p.knobWidth || (p.theme && p.theme.knobWidth) || defaultTheme.knobWidth} -
+              ${p => p.knobGap || (p.theme && p.theme.knobGap) || defaultTheme.knobGap}
+          );
+          transition: all ease-out 0.2s;
           background-color: ${p =>
-  p.knobColor || p.rightKnobColor || (p.theme && p.theme.rightKnobColor) || defaultTheme.rightKnobColor};
+            p.knobColor || p.rightKnobColor || (p.theme && p.theme.rightKnobColor) || defaultTheme.rightKnobColor};
         }
       }
-      
+
       &:disabled {
         & + label {
           background-color: ${p =>
-  p.backgroundColorDisabled ||
-  (p.theme && p.theme.backgroundColorDisabled) ||
-  defaultTheme.backgroundColorDisabled};
+            p.backgroundColorDisabled ||
+            (p.theme && p.theme.backgroundColorDisabled) ||
+            defaultTheme.backgroundColorDisabled};
           &:after {
             box-shadow: none;
           }
         }
       }
     }
-    
+
     // disabled
     &:disabled {
       & + label {
         background-color: ${p =>
-  p.backgroundColorDisabled ||
-  (p.theme && p.theme.backgroundColorDisabled) ||
-  defaultTheme.backgroundColorDisabled};
-        cursor:default;
+          p.backgroundColorDisabled ||
+          (p.theme && p.theme.backgroundColorDisabled) ||
+          defaultTheme.backgroundColorDisabled};
+        cursor: default;
         &:after {
           box-shadow: none;
           background-color: ${p =>
             p.backgroundColorDisabled ||
             (p.theme && p.theme.backgroundColorDisabled) ||
-            defaultTheme.backgroundColorDisabled};;
+            defaultTheme.backgroundColorDisabled};
         }
       }
     }
@@ -315,7 +324,7 @@ const Toggle: FunctionComponent<Props> = props => {
         value={value}
         disabled={disabled}
       />
-      <ToggleContainer htmlFor={name}/>
+      <ToggleContainer htmlFor={name} />
     </ToggleBase>
   );
 };
