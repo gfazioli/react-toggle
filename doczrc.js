6e6d3e0b154ffaad30acb2088ecfbc5719790c26
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2019-05-09 17:16:30 +0200

    rollback docz

--- doczrc.js
+++ doczrc.js
@@ -1,13 +1,14 @@
 // doczrc.js
 
 export default {
-  description : "React Toggle documentation and playground",
-  dest        : "./docs",
-  themeConfig : {
-    colors : {
-      primary : "tomato",
+  description: "React Toggle documentation and playground",
+  dest: "./docs",
+  themeConfig: {
+    colors: {
+      primary: "tomato",
     },
   },
-  title       : "React Toggle",
-  typescript  : true
+  codeSandbox: false,
+  title: "React Toggle",
+  typescript: true
 }
