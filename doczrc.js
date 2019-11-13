82cb0ca8cfe3021082c85c744c1c073c19df126b
John Fazioli <john.fazioli@namecheap.com>
2019-11-13 16:06:04 +0100

    wip

--- doczrc.js
+++ doczrc.js
@@ -3,5 +3,6 @@ export default {
   title: "React Toggle",
   description: "React Toggle documentation and playground",
   base: "/react-toggle",
-  ignore: ["README.md", 'readme.md', 'changelog.md', 'code_of_conduct.md', 'contributing.md', 'license.md']
+  ignore: ["README.md", 'readme.md', 'changelog.md', 'code_of_conduct.md', 'contributing.md', 'license.md'],
+  dest: '/docs',
 }
