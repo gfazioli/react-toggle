328446a8e433d4303b091ee44637cb9f31d7b52d
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-06-12 10:18:35 +0200

    fix(site): use monochrome Tabler coffee icon in donate button

--- site/src/components/Sponsors.tsx
+++ site/src/components/Sponsors.tsx
@@ -50,7 +50,25 @@ export function Sponsors() {
             <span aria-hidden="true">❤</span> Become a sponsor
           </a>
           <a className="btn coffee-btn" href={DONATE_URL} target="_blank" rel="noreferrer noopener">
-            <span aria-hidden="true">☕</span> Buy me a coffee
+            <svg
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M3 14c.83 .642 2.077 1.017 3.5 1c1.423 .017 2.67 -.358 3.5 -1c.83 -.642 2.077 -1.017 3.5 -1c1.423 -.017 2.67 .358 3.5 1" />
+              <path d="M8 3a2.4 2.4 0 0 0 -1 2a2.4 2.4 0 0 0 1 2" />
+              <path d="M12 3a2.4 2.4 0 0 0 -1 2a2.4 2.4 0 0 0 1 2" />
+              <path d="M3 10h14v5a6 6 0 0 1 -6 6h-2a6 6 0 0 1 -6 -6v-5" />
+              <path d="M16.746 16.726a3 3 0 1 0 .252 -5.555" />
+            </svg>
+            Buy me a coffee
           </a>
         </div>
       </div>
